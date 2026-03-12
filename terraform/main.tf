cadddc81b9dadaf2cd7280c74afae036f864f912
avinash-tayade <taydeavi20@gmail.com>
2026-03-12 16:32:54 +0530

    Added docker compose, EC2 resource and README

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,23 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_security_group" "vulnerable_sg" {
-  name        = "devops-vulnerable-sg"
-  description = "Allow SSH from anywhere (intentional vulnerability)"
+resource "aws_security_group" "devops_sg" {
+  name = "devops-assignment-sg"
 
   ingress {
-    description = "SSH from anywhere"
+    description = "SSH open to world (intentional vulnerability)"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # Intentional vulnerability
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App port"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -19,5 +26,17 @@ resource "aws_security_group" "vulnerable_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devops_server" {
+
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "DevOpsAssignmentServer"
   }
 }
